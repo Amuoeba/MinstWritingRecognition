68fdc816bd767109bd213e3c78d0f140e48b2872
Erik Janežič <erik.janezic@gmail.com>
2017-10-11 00:49:04 +0200

    added feedForward method and sigmoid function

--- neural-networks-and-deep-learning-master/my.py
+++ neural-networks-and-deep-learning-master/my.py
@@ -10,3 +10,12 @@ class Network(object):
         self.sizes = sizes
         self.biases = [np.random.rand(y,1) for y in sizes[1:]]
         self.weights = [np.random.rand(y,x) for x,y in zip(sizes[:-1],sizes[1:])]
+        
+    def feedForward(self,a):
+        """Return the output of the network if "a" is the input."""
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w,a)+b)
+        return a
+
+def sigmoid(z):
+    return 1.0/(1.0+np.exp(-z))
